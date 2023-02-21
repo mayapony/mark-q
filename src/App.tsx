6d9ddc36e00e10e9a3340e428798346e8ac37abe
mayapony <1327909321@qq.com>
2023-02-21 22:47:16 +0800

    ✨ feat: add react router support

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <Outlet />;
 }
 
 export default App;
